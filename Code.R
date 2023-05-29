1f4a6fa5c3cd5ca9cb3108f032257c440790c137
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-05-29 14:11:33 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,28 @@
+# Read the CSV files
+x <- read.csv("x.csv", header = FALSE)
+y <- read.csv("y.csv", header = FALSE)
+ 
+# Convert data to numeric
+x <- as.numeric(as.character(x$V1))
+y <- as.numeric(as.character(y$V1))
+ 
+# Perform the Kolmogorov-Smirnov test
+ks_result <- ks.test(x, y)
+ 
+# Print the test result
+print(ks_result)
+
+	
+# visualization
+plot(ecdf(x),
+      xlim = range(c(x, y)),
+      col = "blue")
+ plot(ecdf(y),
+      add = TRUE,
+      lty = "dashed",
+      col = "red")
+ 
+# performing the K-S
+# Test on x and y
+ks.test(x, y, alternative = "l")
+
